e2959b6cbd96a9c82d4e84975b84f21a8debb3cd
Gokul Deepan <gokuldeepan27@gmail.com>
2025-08-09 12:45:53 +0530

    Update backend API URL

--- src/api.js
+++ src/api.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const API_BASE = "https://whatsapp-clone-backend-ytez.onrender.com"; 
 
 export const getChats = () => axios.get(`${API_BASE}/chats`);
-
 export const getMessagesByUser = (wa_id) =>
   axios.get(`${API_BASE}/messages/${wa_id}`);
-
 export const sendMessage = (data) =>
   axios.post(`${API_BASE}/send`, data);
